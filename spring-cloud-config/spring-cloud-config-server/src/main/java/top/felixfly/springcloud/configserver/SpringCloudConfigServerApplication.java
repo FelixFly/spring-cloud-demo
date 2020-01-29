4a09c4377ff3cd15c063fef3c3301080accd396b
FelixFly <chenglinxu@yeah.net>
2020-01-29 17:00:19 +0800

    spring cloud config server

--- spring-cloud-config/spring-cloud-config-server/src/main/java/top/felixfly/springcloud/configserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/top/felixfly/springcloud/configserver/SpringCloudConfigServerApplication.java
@@ -2,6 +2,7 @@ package top.felixfly.springcloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * 配置服务启动类
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/1/29
  */
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
     public static void main(String[] args) {
